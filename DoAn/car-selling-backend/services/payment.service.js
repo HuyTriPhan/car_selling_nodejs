24962c32b82d058acddab81d0d1eac129a9d656d
Phan Huy Tri <144611821+HuyTriPhan@users.noreply.github.com>
2025-04-11 13:11:18 +0700

    Delete DoAn/car-selling-backend/services directory

--- DoAn/car-selling-backend/services/payment.service.js
+++
@@ -1,7 +0,0 @@
-const Payment = require('../models/payment.model');
-
-const createPayment = (data) => new Payment(data).save();
-const getPaymentById = (id) => Payment.findById(id);
-const updatePayment = (id, data) => Payment.findByIdAndUpdate(id, data, { new: true });
-
-module.exports = { createPayment, getPaymentById, updatePayment };
